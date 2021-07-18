c220288c971bb5bfa7b9b7bde4a9897e56c5f6a2
Kyle Seongwoo Jun <njk230@gmail.com>
2021-07-18 13:55:14 +0900

    [main] Add extensions to device_info_plus package

--- lib/apple_product_name.dart
+++ lib/apple_product_name.dart
@@ -1,5 +1,7 @@
 library apple_product_name;
 
+import 'package:device_info_plus/device_info_plus.dart';
+
 part 'apple_product_name.g.dart';
 
 class AppleProductName {
@@ -12,4 +14,20 @@ class AppleProductName {
   String lookup(String machineId) => _lookup(machineId) ?? machineId;
 
   String? lookupOrNull(String machineId) => _lookup(machineId);
+}
+
+extension ProductName1 on IosUtsname {
+  String get productName {
+    if (machine == null) throw ArgumentError.notNull('machine');
+    return AppleProductName().lookup(machine!);
+  }
+
+  String? get productNameOrNull =>
+      machine != null ? AppleProductName().lookupOrNull(machine!) : null;
+}
+
+extension ProductName2 on MacOsDeviceInfo {
+  String get productName => AppleProductName().lookup(model);
+
+  String? get productNameOrNull => AppleProductName().lookupOrNull(model);
 }
