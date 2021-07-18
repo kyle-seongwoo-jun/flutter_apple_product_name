f62217b5282618c53696c4ee23c904bf7bb82158
Kyle Seongwoo Jun <njk230@gmail.com>
2021-07-18 13:02:23 +0900

    [gen] Update gen project

--- gen/main.dart
+++ gen/main.dart
@@ -1,3 +1,40 @@
-void main(List<String> arguments) {
-  print('Hello world!');
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:http/http.dart' as http;
+
+void main() async {
+  print('Generating...');
+
+  final urls = [
+    // iPhone, iPod, iPad, Apple Watch
+    'https://raw.githubusercontent.com/fieldnotescommunities/ios-device-identifiers/master/ios-device-identifiers.json',
+    // iMac, MacBook, MacBook Air, MacBook Pro, Mac mini, Mac Pro
+    'https://raw.githubusercontent.com/kyle-seongwoo-jun/mac-device-identifiers/main/mac-device-identifiers-unique.json',
+  ];
+
+  final responses =
+      await Future.wait(urls.map((url) => http.get(Uri.parse(url))));
+  if (responses.any((response) => response.statusCode != 200)) {
+    print('Request failed');
+    return;
+  }
+
+  final map = responses
+      .map((response) => json.decode(response.body) as Map<String, dynamic>)
+      .fold<Map<String, dynamic>>({}, (a, b) => a..addAll(b));
+
+  final sb = StringBuffer();
+  sb.writeln("part of 'apple_product_name.dart';");
+  sb.writeln('');
+  sb.writeln('const _map = ${json.encode(map)};');
+  sb.writeln('');
+  sb.writeln('String? _lookup(String machineId) => _map[machineId];');
+  sb.writeln('');
+
+  final sourcePath = '../lib/apple_product_name.g.dart';
+  await File(sourcePath).writeAsString(sb.toString());
+  await Process.run('flutter', ['format', sourcePath]);
+
+  print('Generated.');
 }
