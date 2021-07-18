2d19cae475ff5a2a79ac795413686264e4dfa370
Kyle Seongwoo Jun <njk230@gmail.com>
2021-07-18 13:25:14 +0900

    [gen] Update gen project

--- gen/main.dart
+++ gen/main.dart
@@ -8,7 +8,7 @@ void main() async {
 
   final urls = [
     // iPhone, iPod, iPad, Apple Watch
-    'https://raw.githubusercontent.com/fieldnotescommunities/ios-device-identifiers/master/ios-device-identifiers.json',
+    'https://raw.githubusercontent.com/fieldnotescommunities/ios-device-identifiers/c482f86f04e4d509e7ae06b07e82d3c8d3ff318c/ios-device-identifiers.json',
     // iMac, MacBook, MacBook Air, MacBook Pro, Mac mini, Mac Pro
     'https://raw.githubusercontent.com/kyle-seongwoo-jun/mac-device-identifiers/main/mac-device-identifiers-unique.json',
   ];
@@ -24,13 +24,13 @@ void main() async {
       .map((response) => json.decode(response.body) as Map<String, dynamic>)
       .fold<Map<String, dynamic>>({}, (a, b) => a..addAll(b));
 
-  final sb = StringBuffer();
-  sb.writeln("part of 'apple_product_name.dart';");
-  sb.writeln('');
-  sb.writeln('const _map = ${json.encode(map)};');
-  sb.writeln('');
-  sb.writeln('String? _lookup(String machineId) => _map[machineId];');
-  sb.writeln('');
+  final sb = StringBuffer()
+    ..writeln("part of 'apple_product_name.dart';")
+    ..writeln('')
+    ..writeln('const _map = ${json.encode(map)};')
+    ..writeln('')
+    ..writeln('String? _lookup(String machineId) => _map[machineId];')
+    ..writeln('');
 
   final sourcePath = '../lib/apple_product_name.g.dart';
   await File(sourcePath).writeAsString(sb.toString());
